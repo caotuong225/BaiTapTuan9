e32f33103d8e2503351192bc196c78abcb11030c
acer <caotuong226@gmail.com>
2021-06-09 12:57:21 +0700

    Bai Tap

---
+++ app/src/main/java/com/example/tuan9_firebase/Finish.java
@@ -0,0 +1,28 @@
+package com.example.tuan9_firebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Finish extends AppCompatActivity {
+
+    private Button btnFinish;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+
+        btnFinish = findViewById(R.id.btnFinish);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Finish.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
